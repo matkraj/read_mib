e74a441e644031f1ed7784caf689c9f4c7233fb8
Matus Krajnak <krajnak.matus@gmail.com>
2021-04-09 17:43:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,4 @@ import mib
 path = './example.mib'
 data = mib.loadMib(path,scan_size=(2,1))
 
-with open(path,'rb') as f:
-    buffer_mib = f.read()
-
-data_from_buffer = mib.loadMib(buffer_mib,scan_size=(2,1))
-
 print("\nNumpy array from file shape:",data.shape)
-print("Numpy array from buffer shape:",data_from_buffer.shape)
